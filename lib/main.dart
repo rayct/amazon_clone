bc660bc2d97b1d8b7d0ecce369b3b9791b7454b7
rayct <rayct@icloud.com>
2022-06-12 01:44:04 +0100

    Added lens plugin and appbar customization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,14 +14,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amazon Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Rays Amazon Clone'),
         ),
-        body: const Center(
-          child: Text('Flutter Demo Home Page'),
+        body: Column(
+          children: [
+            const Center(
+                child: Text(
+              'Flutter Demo Home Page',
+            )),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click Me'),
+            ),
+          ],
         ),
       ),
     );
