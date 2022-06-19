ab8b13659075b8ac6cab1c6e7dd55c4c8ba6ff1c
rayct <rayct@icloud.com>
2022-06-19 02:46:55 +0100

    Added custom Button and textfiled files

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
-  const CustomTextField({
-    Key? key,
-    required this.controller,
-  }) : super(key: key);
+  final String hintText;
+  const CustomTextField({ Key? key, required this.controller, required this.hintText,}) : super(key: key);
+
+  
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
+        hintText: hintText,
           border: const OutlineInputBorder(
-              hintText: hintText,
-              borderSide: BorderSide(
-                color: Colors.black45,
-              )),
-          enableBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.black45,
+            ),
+          ),
+          enabledBorder: const OutlineInputBorder(
               borderSide: BorderSide(
             color: Colors.black45,
           ))),
