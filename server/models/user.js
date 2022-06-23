bf8b312347a56851230d7429985478540eef3276
rayct <rayct@icloud.com>
2022-06-23 02:42:26 +0100

    Added user model and email validation

--- server/models/user.js
+++ server/models/user.js
@@ -34,3 +34,4 @@ const userSchema = mongoose.Schema({
 });
 
 const User = mongoose.model('User', userSchema);
+// module.exports = authRouter;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const User = require('../models/user');
 
 const authRouter = express.Router();
 
-authRouter.post('/api/signup', (req, res) => {
+authRouter.post('/api/signup', async (req, res) => {
     // Get the data from the client
     const { name, email, password } = req.body;
 
-    
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.json({msg: 'User with the same email already exists!'})
+    }
     // post that data into database
-
     // return that data to the user
 });
 
