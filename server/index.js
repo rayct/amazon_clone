3e748ade8f1745d59260896c5ff4d9f018a37a23
rayct <rayct@icloud.com>
2022-06-23 02:31:44 +0100

    Created DataBase and Users for auth

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // IMPORTS FROM PACKAGES
 const express = require('express');
+const mongoose = require('mongoose')
 
 // IMPORTS FROM OTHER FILES
 const authRouter = require('./routes/auth');
@@ -7,6 +8,18 @@ const authRouter = require('./routes/auth');
 // INIT
 const PORT = 3000;
 const app = express();
+const DB = 'mongodb+srv://ray:xAgxPJg1PTvbv8t4@cluster0.htp6l.mongodb.net/?retryWrites=true&w=majority'
+
+// MIDDLEWARE
+app.use(authRouter);
+
+// Connections
+mongoose.connect(DB).then(() => {
+    console.log('Connection Successful');
+})
+    .catch((e) => {
+        console.log(e);
+    }); 
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`connected at port ${PORT}`);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const authRouter = express.Router();
 
-authRouter.get('/user', (req, res) => {
-  res.json({ msg: 'Ray' });
+authRouter.post('/api/signup', (req, res) => {
+    // Get the data from the client
+    const { name, email, password } = req.body;
+
+    
+    // post that data into database
+
+    // return that data to the user
 });
 
 module.exports = authRouter;
